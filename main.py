100cce8867fc6f4468bab74bd047ad9f8abc5b5d
Tanmay <admin@ADMINs-MacBook-Pro.local>
2025-10-02 13:53:41 +0530

    load patient data in patient.json file and list_patients API

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+# Helper function to load and return the data of patient.json file
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"message": "CampusX is an education platform where you can learn AI"}
+    return {"message": "A fully functional API to manage your patient records"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+
